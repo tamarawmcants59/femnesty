c93142a2d3c64970f640c79bf0aab7f5c8da9933
Ravi S Singh <nits.ravis@gmail.com>
2017-11-15 14:36:49 +0530

    Fixed #6 Cover pic image upload issue

--- src/app/frontend/user/editprofile/editprofile.component.ts
+++ src/app/frontend/user/editprofile/editprofile.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormControl, AbstractControl, FormBuilder, Validators, FormGroup} from '@angular/forms';
+import { FormControl, AbstractControl, FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { UserService } from "../user.service";
-import {ImageCropperComponent, CropperSettings, Bounds} from 'ng2-img-cropper';
+import { ImageCropperComponent, CropperSettings, Bounds } from 'ng2-img-cropper';
 
 
 @Component({
@@ -11,37 +11,37 @@ import {ImageCropperComponent, CropperSettings, Bounds} from 'ng2-img-cropper';
   styleUrls: ['./editprofile.component.css']
 })
 export class EditprofileComponent implements OnInit {
-  @ViewChild('cropper', undefined) 
-  cropper:ImageCropperComponent;
-  public activeTab:string ='activity';
-  public aboutActiveTab:string ='overview';
-  public showImgDive:boolean =false;
-  public showCoverImgDive:boolean =false;
-  public showPostImgDive:boolean =false;
-  public form:FormGroup;
-  public postform:FormGroup;
+  @ViewChild('cropper', undefined)
+  cropper: ImageCropperComponent;
+  public activeTab: string = 'activity';
+  public aboutActiveTab: string = 'overview';
+  public showImgDive: boolean = false;
+  public showCoverImgDive: boolean = false;
+  public showPostImgDive: boolean = false;
+  public form: FormGroup;
+  public postform: FormGroup;
   returnUrl: string;
-  errorMsg: string='';
-  successMsg: string='';
+  errorMsg: string = '';
+  successMsg: string = '';
   public loading = false;
   public loginUserDet: Object = {};
-  public userPostList =[];
-  public userFrndList =[];
-  
+  public userPostList = [];
+  public userFrndList = [];
+
   prfImageData: any;
   coverImageData: any;
   postImgData: any;
   prfCropperSettings: CropperSettings;
   coverCropperSettings: CropperSettings;
-  
+
   constructor(
-    private builder:FormBuilder, 
-    private dataService: UserService, 
+    private builder: FormBuilder,
+    private dataService: UserService,
     private route: ActivatedRoute,
     private router: Router
-  ) { 
+  ) {
     //super();
-    let getUserDet = localStorage.getItem("currentUser");
+    const getUserDet = localStorage.getItem("currentUser");
     this.loginUserDet = JSON.parse(getUserDet);
     this.prfCropperSettings = new CropperSettings();
     this.prfCropperSettings.width = 200;
@@ -79,23 +79,23 @@ export class EditprofileComponent implements OnInit {
     this.coverImageData = {};
 
     this.form = builder.group({
-			first_name: ['', [
-				Validators.required,
-				Validators.minLength(3)
-			]],
-			last_name: ['', [
-				Validators.required,
-				Validators.minLength(3)
-			]],
+      first_name: ['', [
+        Validators.required,
+        Validators.minLength(3)
+      ]],
+      last_name: ['', [
+        Validators.required,
+        Validators.minLength(3)
+      ]],
 			/*email: ['', [
 				Validators.required,
 				//BasicValidators.email
 				Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
 			]],*/
-			mobile_number: ['', [
-			   Validators.required,
-			   Validators.minLength(10)
-			 ]],
+      mobile_number: ['', [
+        Validators.required,
+        Validators.minLength(10)
+      ]],
 			/*dob: ['', [
 			   Validators.required,
 			   Validators.minLength(3)
@@ -104,44 +104,44 @@ export class EditprofileComponent implements OnInit {
 			   Validators.required,
 			   Validators.minLength(3)
 			 ]],*/
-			state: ['', [
-			   Validators.required,
-			   Validators.minLength(3)
-			 ]],
-       city: ['', [
-			   Validators.required,
-			   Validators.minLength(3)
-			 ]],
+      state: ['', [
+        Validators.required,
+        Validators.minLength(3)
+      ]],
+      city: ['', [
+        Validators.required,
+        Validators.minLength(3)
+      ]],
       address: ['', [
-			   Validators.required,
-			   Validators.minLength(3)
-       ]],
-       occupation: ['', [
-			   Validators.required,
-			   //Validators.minLength(3)
-			 ]],
-       bio: ['', [
-			   //Validators.required,
-			   //Validators.minLength(3)
-			 ]]
+        Validators.required,
+        Validators.minLength(3)
+      ]],
+      occupation: ['', [
+        Validators.required,
+        //Validators.minLength(3)
+      ]],
+      bio: ['', [
+        //Validators.required,
+        //Validators.minLength(3)
+      ]]
 
     });
 
     this.postform = builder.group({
-			description: ['', [
-				Validators.required,
-				Validators.minLength(3)
-			]],
+      description: ['', [
+        Validators.required,
+        Validators.minLength(3)
+      ]],
       file_name: ['', [
-			   //Validators.required,
-			   //Validators.minLength(3)
-			 ]],
-       is_connection: ['', [
         //Validators.required,
         //Validators.minLength(3)
-      ]] 
+      ]],
+      is_connection: ['', [
+        //Validators.required,
+        //Validators.minLength(3)
+      ]]
     });
-    
+
     //console.log(this.cropper);
   }
 
@@ -152,250 +152,250 @@ export class EditprofileComponent implements OnInit {
     this.getConnectionList();
   }
 
-  public getConnectionList(){
-      let loginUserId=localStorage.getItem("loginUserId");
-      if(loginUserId!=''){
-          let dataUserDet ={
-            "user_id": loginUserId
-          };
-          this.dataService.getUserFrndListById(dataUserDet)
-          .subscribe(data => {
-                let details=data;
-                //console.log(details);
-                if (details.Ack=="1") {
-                  this.userFrndList = details.FriendListById;
-                }else{
-                  
-                }
-            },
-            error => {
-              
-            }
-          ); 
-        }else{
+  public getConnectionList() {
+    const loginUserId = localStorage.getItem("loginUserId");
+    if (loginUserId != '') {
+      const dataUserDet = {
+        "user_id": loginUserId
+      };
+      this.dataService.getUserFrndListById(dataUserDet)
+        .subscribe(data => {
+          const details = data;
+          //console.log(details);
+          if (details.Ack == "1") {
+            this.userFrndList = details.FriendListById;
+          } else {
+
+          }
+        },
+        error => {
+
         }
+        );
+    } else {
+    }
 
   }
 
-  public getUserDetails(){
-    let loginUserId=localStorage.getItem("loginUserId");
-    if(loginUserId!=''){
-        let dataUserDet ={
-          "id": loginUserId
-        };
-        this.dataService.getUserDetById(dataUserDet)
+  public getUserDetails() {
+    const loginUserId = localStorage.getItem("loginUserId");
+    if (loginUserId != '') {
+      const dataUserDet = {
+        "id": loginUserId
+      };
+      this.dataService.getUserDetById(dataUserDet)
         .subscribe(data => {
-              let details=data;
-              //console.log(details);
-              if (details.Ack=="1") {
-                this.loginUserDet = details.UserDetails[0];
-              }else{
-                
-              }
-          },
-          error => {
-            
+          const details = data;
+          //console.log(details);
+          if (details.Ack == "1") {
+            this.loginUserDet = details.UserDetails[0];
+          } else {
+
           }
-        ); 
-      }else{
-      }
+        },
+        error => {
 
-    }
-
-    public getUserPostDetails(){
-      let loginUserId=localStorage.getItem("loginUserId");
-      if(loginUserId!=''){
-          let dataUserDet ={
-            "user_id": loginUserId
-          };
-          this.dataService.getUserPostById(dataUserDet)
-          .subscribe(data => {
-                let details=data;
-                if (details.Ack=="1") {
-                  this.userPostList = details.ActivePostByUser;
-                  //console.log(this.userPostList);
-                }else{
-                  
-                }
-            },
-            error => {
-              
-            }
-          ); 
-        }else{
         }
-  
-      }
+        );
+    } else {
+    }
 
-    public updateAccount() {
-      this.loading = true;
-      let loginUserId=localStorage.getItem("loginUserId");
-      var result,
-              userValue = this.form.value;
-      userValue.id = loginUserId;        
-      //console.log(userValue);  
-      this.dataService.updateAccountDet(userValue)
-        .subscribe(
-              data => {
+  }
 
-                  let details = data;
-                  localStorage.setItem('currentUser', JSON.stringify(details.UserDetails));
-                  localStorage.setItem('userName', details.UserDetails.first_name);
-                  localStorage.setItem('profile_image', details.UserDetails.image_url);
-                  this.loading = false;
-                  this.successMsg='Data updated successfully';
-                  //this.router.navigateByUrl('/user/profile');
-                  this.router.navigate(['/user/profile']);
-            
+  public getUserPostDetails() {
+    const loginUserId = localStorage.getItem("loginUserId");
+    if (loginUserId != '') {
+      const dataUserDet = {
+        "user_id": loginUserId
+      };
+      this.dataService.getUserPostById(dataUserDet)
+        .subscribe(data => {
+          const details = data;
+          if (details.Ack == "1") {
+            this.userPostList = details.ActivePostByUser;
+            //console.log(this.userPostList);
+          } else {
+
+          }
         },
         error => {
-          alert(error);
-        });
-  
+
+        }
+        );
+    } else {
     }
-    
-    public submitPost() {
-      this.loading = true;
-      let loginUserId=localStorage.getItem("loginUserId");
-      var result,
+
+  }
+
+  public updateAccount() {
+    this.loading = true;
+    const loginUserId = localStorage.getItem("loginUserId");
+    const result = {},
+      userValue = this.form.value;
+    userValue.id = loginUserId;
+    this.dataService.updateAccountDet(userValue)
+      .subscribe(
+      data => {
+
+        const details = data;
+        localStorage.setItem('currentUser', JSON.stringify(details.UserDetails));
+        localStorage.setItem('userName', details.UserDetails.first_name);
+        localStorage.setItem('profile_image', details.UserDetails.image_url);
+        this.loading = false;
+        this.successMsg = 'Data updated successfully';
+        //this.router.navigateByUrl('/user/profile');
+        this.router.navigate(['/user/profile']);
+
+      },
+      error => {
+        alert(error);
+      });
+
+  }
+
+  public submitPost() {
+    this.loading = true;
+    const loginUserId = localStorage.getItem("loginUserId");
+    const result = {},
       userValue = this.postform.value;
-      userValue.user_id = loginUserId;   
-      userValue.file_name = this.postImgData;      
-      //console.log(userValue);  
-      
-      this.dataService.postDataSend(userValue)
-        .subscribe(
-              data => {
-                  this.showPostImgDive=false;
-                  //let details = data;
-                  this.loading = false;
-                  this.successMsg='Successfully post data';
-                  this.getUserPostDetails();
-                  this.postform.reset();
-                
-                  //window.location.reload();
-                  //this.router.navigateByUrl('/user/profile');
-                  //this.router.navigate(['/user/profile']);
-            
-        },
-        error => {
-          alert(error);
-        });
-  
-    }
+    userValue.user_id = loginUserId;
+    userValue.file_name = this.postImgData;
 
-    public fileChangeListener($event) {
-        this.showImgDive=true;
-        var image:any = new Image();
-        var file:File = $event.target.files[0];
-        var myReader:FileReader = new FileReader();
-        var that = this;
-        myReader.onloadend = function (loadEvent:any) {
-            image.src = loadEvent.target.result;
-            that.cropper.setImage(image);
-            //console.log(that.cropper);
-        };
-        myReader.readAsDataURL(file);
-    }
-    
-    public fileChangeListenerCover($event) {
-        this.showCoverImgDive=true;
-        var image:any = new Image();
-        var file:File = $event.target.files[0];
-        var myReader:FileReader = new FileReader();
-        var that = this;
-        myReader.onloadend = function (loadEvent:any) {
-            image.src = loadEvent.target.result;
-            that.cropper.setImage(image);
-            //console.log(that.cropper);
-        };
-        myReader.readAsDataURL(file);
-    }
-    
-    public fileChangePost($event) {
-        this.showPostImgDive=true;
-        var image:any = new Image();
-        var file:File = $event.target.files[0];
-        var myReader:FileReader = new FileReader();
-        var that = this;
-        myReader.onloadend = function (loadEvent:any) {
-            image.src = loadEvent.target.result;
-            that.postImgData= image.src;
-            //that.cropper.setImage(image);
-            //console.log(image.src);
-        };
-        myReader.readAsDataURL(file);
-    }
+    this.dataService.postDataSend(userValue)
+      .subscribe(
+      data => {
+        this.showPostImgDive = false;
+        //let details = data;
+        this.loading = false;
+        this.successMsg = 'Successfully post data';
+        this.getUserPostDetails();
+        this.postform.reset();
 
-    public cropped(bounds:Bounds) {
-        //console.log(bounds);
-    }
+        //window.location.reload();
+        //this.router.navigateByUrl('/user/profile');
+        //this.router.navigate(['/user/profile']);
 
-    public UploadPrfImg() {
-      //console.log(this.cropper.image.image);
-      this.loading = true;
-      let loginUserId=localStorage.getItem("loginUserId");
-      let uploadJsonData={
-        "id": loginUserId, 
-        "profile_image": this.cropper.image.image
-       };
-       //console.log(uploadJsonData);
-       this.dataService.updateImgService(uploadJsonData)
-       .subscribe(
-             data => {
-              this.loading = false;
-              this.showImgDive = false;
-              this.getUserDetails();
-              this.router.navigateByUrl('/user/profile');
-                 //let details = data;
-                 //localStorage.setItem('profile_image', details.UserDetails.image_url);
-                 //this.successMsg='Data updated successfully';
-                 //this.router.navigateByUrl('/user/profile');
-       },
-       error => {
-         alert(error);
-       });
-       
-        //console.log(this.cropper.image);
-    }
-    
-    public UploadCoverImg() {
-      //console.log(this.cropper.image.image);
-      this.loading = true;
-      let loginUserId=localStorage.getItem("loginUserId");
-      let uploadJsonData={
-        "id": loginUserId, 
-        "cover_img": this.cropper.image.image
-       };
-       //console.log(uploadJsonData);
-       this.dataService.updateImgService(uploadJsonData)
-       .subscribe(
-             data => {
-              this.loading = false;
-              this.showCoverImgDive = false;
-              this.getUserDetails();
-              //this.router.navigateByUrl('/user/profile');
-       },
-       error => {
-         alert(error);
-       });
-    }
+      },
+      error => {
+        alert(error);
+      });
 
-    /*public onReturnData(data: any) {
-        // Do what you want to do
-        console.warn(JSON.parse(data));
-    }*/
-    public toggleTab(data: any){
-        //console.log(data);
-        this.activeTab=data;
-        this.successMsg='';
-        this.errorMsg='';
-    }
+  }
 
-    public aboutToggleTab(data: any){
-        this.successMsg='';
-        this.errorMsg='';
-        this.aboutActiveTab=data;
-    }
-    
+  public fileChangeListener($event) {
+    this.showImgDive = true;
+    const image: any = new Image();
+    const file: File = $event.target.files[0];
+    const myReader: FileReader = new FileReader();
+    const that = this;
+    myReader.onloadend = function (loadEvent: any) {
+      image.src = loadEvent.target.result;
+      that.cropper.setImage(image);
+      //console.log(that.cropper);
+    };
+    myReader.readAsDataURL(file);
+  }
+
+  public fileChangeListenerCover($event) {
+    this.showCoverImgDive = true;
+    const image: any = new Image();
+    const file: File = $event.target.files[0];
+    const myReader: FileReader = new FileReader();
+    const that = this;
+    myReader.onloadend = function (loadEvent: any) {
+      image.src = loadEvent.target.result;
+      that.cropper.setImage(image);
+      //console.log(that.cropper);
+    };
+    myReader.readAsDataURL(file);
+  }
+
+  public fileChangePost($event) {
+    this.showPostImgDive = true;
+    const image: any = new Image();
+    const file: File = $event.target.files[0];
+    const myReader: FileReader = new FileReader();
+    const that = this;
+    myReader.onloadend = function (loadEvent: any) {
+      image.src = loadEvent.target.result;
+      that.postImgData = image.src;
+      //that.cropper.setImage(image);
+      //console.log(image.src);
+    };
+    myReader.readAsDataURL(file);
+  }
+
+  public cropped(bounds: Bounds) {
+    //console.log(bounds);
+  }
+
+  public UploadPrfImg() {
+    //console.log(this.cropper.image.image);
+    this.loading = true;
+    const loginUserId = localStorage.getItem("loginUserId");
+    const uploadJsonData = {
+      "id": loginUserId,
+      "profile_image": this.cropper.image.image
+    };
+    //console.log(uploadJsonData);
+    this.dataService.updateImgService(uploadJsonData)
+      .subscribe(
+      data => {
+        this.loading = false;
+        this.showImgDive = false;
+        this.getUserDetails();
+        window.location.reload();
+        //this.router.navigateByUrl('/user/profile');
+        //let details = data;
+        //localStorage.setItem('profile_image', details.UserDetails.image_url);
+        //this.successMsg='Data updated successfully';
+        //this.router.navigateByUrl('/user/profile');
+      },
+      error => {
+        alert(error);
+      });
+
+    //console.log(this.cropper.image);
+  }
+
+  public UploadCoverImg() {
+    //console.log(this.cropper.image.image);
+    this.loading = true;
+    const loginUserId = localStorage.getItem("loginUserId");
+    const uploadJsonData = {
+      "id": loginUserId,
+      "cover_img": this.cropper.image.image
+    };
+    //console.log(uploadJsonData);
+    this.dataService.updateImgService(uploadJsonData)
+      .subscribe(
+      data => {
+        this.loading = false;
+        this.showCoverImgDive = false;
+        this.getUserDetails();
+        window.location.reload();
+        //this.router.navigateByUrl('/user/profile');
+      },
+      error => {
+        alert(error);
+      });
+  }
+
+  /*public onReturnData(data: any) {
+      // Do what you want to do
+      console.warn(JSON.parse(data));
+  }*/
+  public toggleTab(data: any) {
+    //console.log(data);
+    this.activeTab = data;
+    this.successMsg = '';
+    this.errorMsg = '';
+  }
+
+  public aboutToggleTab(data: any) {
+    this.successMsg = '';
+    this.errorMsg = '';
+    this.aboutActiveTab = data;
+  }
+
 }
