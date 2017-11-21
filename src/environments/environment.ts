5308b78f9903c5a2bd909be63f38cf34318854fb
zindia16 <zindia.in@gmail.com>
2017-11-21 18:55:05 +0530

    Footer chat changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,10 +3,19 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+// export const environment = {
+//   production: false,
+//   api_url: 'http://192.168.1.118/team2/Femnesty/webservice',
+//   site_url: 'http://192.168.1.118/team2/Femnesty/',
+//   DATE_FMT: 'dd/MMM/yyyy',
+//   DATE_TIME_FMT:'dd/MMM/yyyy hh:mm:ss',
+// };
+
 export const environment = {
   production: false,
-  api_url: 'http://192.168.1.118/team2/Femnesty/webservice',
+  api_url: 'http://111.93.169.90/team2/Femnesty/webservice',
   site_url: 'http://192.168.1.118/team2/Femnesty/',
   DATE_FMT: 'dd/MMM/yyyy',
   DATE_TIME_FMT:'dd/MMM/yyyy hh:mm:ss',
 };
+
