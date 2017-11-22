00b652d2d612b945e5c6a8a608595409141db48e
Suman Samanta <nits.abhishek85@gmail.com>
2017-11-22 15:02:30 +0530

    group data issue

--- src/app/components/group-rightbar/group-rightbar.component.ts
+++ src/app/components/group-rightbar/group-rightbar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { UserService } from "../../frontend/user/user.service";
+//import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-group-rightbar',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupRightbarComponent implements OnInit {
 
-  constructor() { }
+  public isloginUserId: string = '';
+  public isUserLogin: string = '';
 
-  ngOnInit() {
+  @Input() groupData: {
+    id: number;
+    user_id: number;
+    slug: string;
+    group_uname: string;
+    group_name: string;
+    short_desc: string;
+    long_desc: string;
+    status: string;
+    first_name: string;
+    last_name: string;
+    name: string;
+    profile_image: string;
+    display_name: string;
+    group_image: string;
+    member_count: number;
+    cdate: string;
+  };
+
+  constructor(
+    private dataService: UserService
+  ) {
+    this.isloginUserId = localStorage.getItem("loginUserId");
+    this.isUserLogin = localStorage.getItem("isLoggedIn");
   }
 
+  ngOnInit() {
+    if(this.groupData.id){
+        console.log(this.groupData);
+    }
+  }
+  
 }
--- src/app/frontend/user/shareyourstory/shareyourstory.component.ts
+++ src/app/frontend/user/shareyourstory/shareyourstory.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from "../user.service";
 
 @Component({
   selector: 'app-shareyourstory',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShareyourstoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private dataService: UserService
+  ) { }
 
   ngOnInit() {
   }
