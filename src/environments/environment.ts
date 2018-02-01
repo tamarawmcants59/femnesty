d69e7e890e392e9e35b528bb8f10f2b8eabcd143
unknown <soutik@natitsolved.com>
2018-02-01 16:15:24 +0530

    01-02-2018/16:15

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,7 @@
 //   DATE_TIME_FMT:'dd/MMM/yyyy hh:mm:ss',
 // };
 
-/*export const environment = {
+export const environment = {
   production: false,
   api_url: 'http://111.93.169.90/team2/Femnesty/webservice',
   site_url: 'http://111.93.169.90/team2/Femnesty/',
@@ -19,7 +19,7 @@
   DATE_FMT: 'dd/MMM/yyyy',
   DATE_TIME_FMT:'dd/MMM/yyyy hh:mm:ss',
 };
-*/
+/*
 export const environment = {
   production: false,
   api_url: 'http://51.140.27.84/admin/webservice',
@@ -27,4 +27,4 @@ export const environment = {
   website_url:'http://51.140.27.84/beta/#',
   DATE_FMT: 'dd/MMM/yyyy',
   DATE_TIME_FMT:'dd/MMM/yyyy hh:mm:ss',
-};
+};*/
