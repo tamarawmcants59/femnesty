2a5e01dc946655b7dca25cf1e8950c4eb0247f75
nitspalash <palash@natitsolved.com>
2018-02-08 21:46:40 +0530

    profile edit

--- src/app/frontend/group/group.component.ts
+++ src/app/frontend/group/group.component.ts
@@ -91,6 +91,7 @@ public invitediv = '';
     this.isUserLogin = localStorage.getItem("isLoggedIn");
     this.invitediv = localStorage.getItem("invitediv");
 
+    localStorage.setItem("invitediv",'');
 
     this.form = builder.group({
       group_name: ['', [
