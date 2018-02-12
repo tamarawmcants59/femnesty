b384427cc53fefcff1ee94abdde90384cc5cf1e7
krishnendu <krishnendu@natitsolved.com>
2018-02-12 12:02:53 +0530

    chat listing and latest chat

--- src/app/components/frontend-app-header/frontend-app-header.component.ts
+++ src/app/components/frontend-app-header/frontend-app-header.component.ts
@@ -17,7 +17,7 @@ import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 })
 export class FrontendAppHeader {
   pageConData = [];
-  artCatData=[];
+  artCatData = [];
   public loginForm: FormGroup;
   HeaderNavCls: string = '';
   HeaderTopCls: string = '';
@@ -173,7 +173,7 @@ export class FrontendAppHeader {
     /*if (this.userloggedIn == '1') {
       this.userNotiCountList();
     }*/
-    
+
     this.getUserDetails();
     this.getArticleList();
     this.getUnreadMessages();
@@ -183,15 +183,15 @@ export class FrontendAppHeader {
     this.show = false;
   }
 
-  public getFemnestyEvent(){
-    this._service.getArticalListData().subscribe(data=>{
-        let details=data;
-        //console.log(details.EventList[0]);
-        if (details.Ack=="1") {
-            //this.artDetData = details.BooksAll[0].image_url;
-            this.artCatData = details.EventList[0];
-        }
-      },
+  public getFemnestyEvent() {
+    this._service.getArticalListData().subscribe(data => {
+      let details = data;
+      //console.log(details.EventList[0]);
+      if (details.Ack == "1") {
+        //this.artDetData = details.BooksAll[0].image_url;
+        this.artCatData = details.EventList[0];
+      }
+    },
       error => {
         console.log('Something went wrong!');
       });
@@ -248,58 +248,148 @@ export class FrontendAppHeader {
 
   }
   getUnreadMessages() {
-    var newArray=[];
+    // var newArray=[];
+    // const messages = this.db.collection('Messages', ref => {
+    //   return ref.where('to_user_id', '==', this.loginUserId).orderBy('created','asc');
+    // }).snapshotChanges().map(actions => {
+    //   debugger;
+    //   return actions.map(action => {
+    //     debugger
+    //     const data = action.payload.doc.data();
+    //     var isFound=false;
+    //     var index;
+    //     if(newArray.length>0)
+    //     {
+    //       for(var i=0;i<newArray.length;i++)
+    //       {
+    //         if(newArray[i].from_user_id==data.from_user_id)
+    //         {
+    //           isFound=true;
+    //           index=i;
+    //           break;
+    //         }
+    //       }
+    //       if(isFound)
+    //       {
+    //          if(index)
+    //          {
+    //            newArray[index].message.text=data.message.text;
+    //          }
+    //          else if(index==0)
+    //          {
+    //           newArray[index].message.text=data.message.text;
+    //          }
+
+    //       }
+    //       else
+    //       {
+    //         newArray.push(data);
+    //       }
+    //     }
+    //     else
+    //     {
+    //       newArray.push(data);
+    //     }
+
+    //     const id = action.payload.doc.id;
+    //     return { id, ...data };
+    //   });
+    // });
+    // messages.subscribe(data => {
+    //   debugger;
+    //   if(newArray && newArray.length)
+    //   {
+    //     this.chatHeads=newArray;
+    //   }
+    //   else
+    //   this.chatHeads = data;
+    //   this.fillUserDetails();
+    // });
+    var newArray = [];
     const messages = this.db.collection('Messages', ref => {
-      return ref.where('to_user_id', '==', this.loginUserId);
+      return ref.where('to_user_id', '==', this.loginUserId).orderBy('created', 'desc');
     }).snapshotChanges().map(actions => {
       return actions.map(action => {
         const data = action.payload.doc.data();
-        var isFound=false;
-        var index;
-        if(newArray.length>0)
-        {
-          for(var i=0;i<newArray.length;i++)
-          {
-            if(newArray[i].from_user_id==data.from_user_id)
-            {
-              isFound=true;
-              index=i;
-              break;
-            }
-          }
-          if(isFound)
-          {
-             if(index)
-             {
-               newArray[index].message.text=data.message.text;
-             }
-             else if(index==0)
-             {
-              newArray[index].message.text=data.message.text;
-             }
-             
-          }
-          else
-          {
-            newArray.push(data);
-          }
-        }
-        else
-        {
-          newArray.push(data);
-        }
-        
+        // var isFound=false;
+        // var index;
+        // if(newArray.length>0)
+        // {
+        //   for(var i=0;i<newArray.length;i++)
+        //   {
+        //     if(newArray[i].from_user_id==data.from_user_id)
+        //     {
+        //       isFound=true;
+        //       index=i;
+        //       break;
+        //     }
+        //   }
+        //   if(isFound)
+        //   {
+        //      if(index)
+        //      {
+        //        newArray[index].message.text=data.message.text;
+        //      }
+        //      else if(index==0)
+        //      {
+        //       newArray[index].message.text=data.message.text;
+        //      }
+
+        //   }
+        //   else
+        //   {
+        //     newArray.push(data);
+        //   }
+        // }
+        // else
+        // {
+        //   newArray.push(data);
+        // }
+        newArray.push(data);
+
         const id = action.payload.doc.id;
         return { id, ...data };
       });
     });
     messages.subscribe(data => {
-      if(newArray && newArray.length)
-      {
-        this.chatHeads=newArray;
+      // if(newArray && newArray.length)
+      // {
+      //   this.chatHeads=newArray;
+      // }
+      // else
+      // this.chatHeads = data;
+      
+      var isFound = false;
+      var index;
+      var finalArray = [];
+      if (data && data.length > 0) {
+        for (var i = 0; i < data.length; i++) {
+          if (i == 0) {
+            finalArray.push(data[i]);
+          }
+          else {
+            var isfound=false;
+            for (var j = 0; j < finalArray.length; j++) {
+              if (finalArray[j].from_user_id == data[i]["from_user_id"]) {
+                isfound=true;
+                break;
+              }
+              else {
+                isfound=false;
+                //finalArray.push(data[i]);
+              }
+            }
+            if(isfound==false)
+            {
+              finalArray.push(data[i]);
+            }
+          }
+        }
+        this.chatHeads = finalArray;
       }
       else
-      this.chatHeads = data;
+        this.chatHeads = data;
+
       this.fillUserDetails();
     });
   }
